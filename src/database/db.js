51f856fea2bd52afc033cc805791b1faf639ea14
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-10 09:42:25 -0300

    create database connection

---
+++ src/database/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+function conect() { 
+  mongoose.set('useNewUrlParser', true);
+  mongoose.set('useUnifiedTopology', true);
+
+  mongoose.connect('mongodb://localhost:27017/api-restful-course?readPreference=primary&appname=MongoDB%20Compass&ssl=false');
+
+  const db = mongoose.connection;
+
+  db.once('open', () => {
+    console.log('DB is connected');
+  });
+
+  db.on('error', () => {
+    console.error.bind(console, 'Connection error: ');
+  })
+}
+
+module.exports = {
+  conect,
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const routes = require('./routes/routes');
+const db = require('./database/db');
 
 const app = express();
+
+db.conect();
 
 app.use('/api', routes);
 
