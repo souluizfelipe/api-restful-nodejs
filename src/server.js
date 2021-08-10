fe246bbb0309477880201f388bf1a2786dd55ddf
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-10 08:59:25 -0300

    create first api route and basic project struture

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const routes = require('./router/routes');
+const routes = require('./routes/routes');
 
 const app = express();
 
-app.use('/', routes);
+app.use('/api', routes);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`server on port ${port} working...`));
