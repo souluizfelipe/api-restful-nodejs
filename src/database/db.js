0ef41489f66fdf8df7c067b22a29ab1448637e86
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-10 09:45:34 -0300

    change mongoose parameters

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 function conect() { 
-  mongoose.set('useNewUrlParser', true);
-  mongoose.set('useUnifiedTopology', true);
-
-  mongoose.connect('mongodb://localhost:27017/api-restful-course?readPreference=primary&appname=MongoDB%20Compass&ssl=false');
+  
+  mongoose.connect('mongodb://localhost:27017/api-restful-course?readPreference=primary&appname=MongoDB%20Compass&ssl=false', 
+    {useNewUrlParser: true}, 
+    {useUnifiedTopology: true}
+  );
 
   const db = mongoose.connection;
 
