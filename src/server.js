1c0ffc93be030aba3da0a88a620f4731b4aa9626
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-10 08:38:46 -0300

    add basic server config

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('running...')
+const express = require('express');
+
+const routes = require('./router/routes');
+
+const app = express();
+
+app.use('/', routes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`server on port ${port} working...`));
