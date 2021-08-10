b2a63fc93fee694f74ed8b9daf5d79dd81920b0e
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-10 18:20:29 -0300

    add cors to project

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes/routes');
 const db = require('./database/db');
+const { all } = require('./routes/routes');
 
 const app = express();
 
 db.conect();
+
+
+const allowedOrigins = [
+  'http://127.0.0.1:5500'
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    let allowed = true;
+
+    if(!origin) allowed = true;
+
+    if(!allowedOrigins.includes(origin)) allowed = false;
+
+    callback(null, allowed);
+  },
+}));
+
 
 app.use(express.json());
 
